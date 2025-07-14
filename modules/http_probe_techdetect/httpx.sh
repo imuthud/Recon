ba0adb0f8d91fd4a25ae5bfbe234bbd7bb9b278d
Muthu D <newtestbug29+1@gmail.com>
2025-07-15 01:00:35 +0530

    Update httpx.sh

--- modules/http_probe_techdetect/httpx.sh
+++ modules/http_probe_techdetect/httpx.sh
@@ -2,4 +2,4 @@
 input="output/resolved_subdomains.txt"
 mkdir -p output
 
-cat $input | httpx -silent -title -status-code -tech-detect -o output/httpx_info.txt
+cat $input | httpx-toolkit -silent -title -status-code -tech-detect -o output/httpx_info.txt
