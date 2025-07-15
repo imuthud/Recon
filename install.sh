5b5b18b19c6feb42e513ad1d7e0e737fce5228a9
Muthu D <newtestbug29+1@gmail.com>
2025-07-15 12:36:53 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+echo "[*] Adding Go binary path to shell config..."
+echo 'export PATH=$PATH:$HOME/go/bin' >> ~/.bashrc
+source ~/.bashrc
+
+echo "[*] Installing Go-based tools..."
 go install github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 go install github.com/tomnomnom/assetfinder@latest
 go install github.com/OWASP/Amass/v3/...@master
@@ -10,4 +16,8 @@ go install github.com/projectdiscovery/katana/cmd/katana@latest
 go install github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
 go install github.com/projectdiscovery/httpx/cmd/httpx@latest
 go install github.com/Emoe/kxss@latest
-pip install -r tools/LinkFinder/requirements.txt
+
+echo "[*] Installing Python dependencies for LinkFinder..."
+pip install -r tools/LinkFinder/requirements.txt --break-system-packages
+
+echo "[✔] All tools installed successfully and globally available."
