b7bdb252b713e2cc6fdac79b2718b0cc8e793e4a
Muthu D <newtestbug29+1@gmail.com>
2025-07-15 17:01:13 +0530

    Update sensitive_file_grep.sh

--- modules/sensitive_file_check/sensitive_file_grep.sh
+++ modules/sensitive_file_check/sensitive_file_grep.sh
@@ -3,4 +3,4 @@ input="output/all_urls.txt"
 output="output/sensitive_files.txt"
 mkdir -p output
 
-cat $input | grep -Ei '\.(git|env|bak|log|sql|xml|pdf|docx?|xlsx?)($|\?)' > $output
+cat $input | grep -Ei '\.(git|env|bak|log|sql|xml|pdf|csv|docx?|xlsx?)($|\?)' > $output
